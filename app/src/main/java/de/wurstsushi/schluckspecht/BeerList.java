69c4d7f59eaee7319ebe9442235ebe0c4f9301ed
Florian Österreich <florian.oesterreich@googlemail.com>
2017-07-06 17:00:36 +0200

    Add ImageButton for signalling the absence of beer

--- app/src/main/java/de/wurstsushi/schluckspecht/BeerList.java
+++ app/src/main/java/de/wurstsushi/schluckspecht/BeerList.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageButton;
 
 public class BeerList extends AppCompatActivity {
 
@@ -23,7 +24,16 @@ public class BeerList extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            }
+        });
+
+        ImageButton button = (ImageButton) findViewById(R.id.buttonBeerLeer);
+        button.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                Snackbar.make(view, "Es gib kein Bier mehr, es muss Bier her!", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                return true;
             }
         });
     }
